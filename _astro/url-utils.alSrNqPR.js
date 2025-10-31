a7242ad6cae293883f54d76dddbbb163394531df
Zuoyan233 <zuoxiju233@gmail.com>
2025-10-31 12:23:09 +0000

    Deploying to pages from @ Zuoyan233/Zuoyanblogs@b4b1d089f0b86064a1c4af598fb0f8776256d9c5 🚀

---
+++ _astro/url-utils.alSrNqPR.js
@@ -0,0 +1,1 @@
+import"./config.Q9z6cz-R.js";import"./zh_TW.LAM-gO2q.js";function u(o,t){const n=o.replace(/^\/|\/$/g,"").toLowerCase(),r=t.replace(/^\/|\/$/g,"").toLowerCase();return n===r}function e(...o){return o.join("/").replace(/\/+/g,"/")}function c(o){return a(`/posts/${o}/`)}function a(o){return e("","/",o)}export{c as g,u as p,a as u};
