7fb32d0895b4883bb332fa5a603609d3d0c78209
Zuoyan233 <zuoxiju233@gmail.com>
2026-01-21 20:22:53 +0800

    修复剩余的语法错误

--- src/global.d.ts
+++ src/global.d.ts
@@ -19,30 +19,6 @@ declare global {
 			}>;
 		};
 
-		translate?: {
-			service: {
-				use: (service: string) => void;
-			};
-			language: {
-				setLocal: (language: string) => void;
-			};
-			setAutoDiscriminateLocalLanguage: () => void;
-			ignore: {
-				class: string[];
-				tag: string[];
-			};
-			selectLanguageTag: {
-				show: boolean;
-			};
-			storage: {
-				set: () => void;
-			};
-			listener: {
-				start: () => void;
-			};
-			execute: () => void;
-		};
-
 		mobileTOCInit?: () => void;
 		initSemifullScrollDetection?: () => void;
 		iconifyLoaded?: boolean;
